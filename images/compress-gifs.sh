b0eed74353ad2c8359751cd5265ef058acd30df9
jmbeach <jaredbeachdesign@gmail.com>
2016-03-19 18:48:13 -0500

    compressed-images

---
+++ images/compress-gifs.sh
@@ -0,0 +1,3 @@
+gifsicle -O3 --lossy=80 -o cond-example-small.gif cond-example.gif
+gifsicle -O3 --lossy=80 -o define-example-small.gif define-example.gif
+gifsicle -O3 --lossy=80 -o lambda-example-small.gif lambda-example.gif
